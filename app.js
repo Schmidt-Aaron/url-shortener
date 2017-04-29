e39bec0e95c1510e3dfc5bff424494c7978c1d6d
Schmidt-Aaron <aschmidt808@gmail.com>
2017-04-29 12:12:41 -0700

    added modules and basic mongo support

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
+//load express and other dependencies
 var express = require('express');
 var app = express();
+
+//fire up mongo
 var mongo = require('mongodb');
+var MongoClient = mongo.MongoClient;
+
+//where the database is
+var url = 'mongodb://localhost:27017/database_name';
+
+MongoClient.connect(url, function(err, db){
+    if(err) {
+        console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+        console.log('Connection established to', url);
+    }
+
+    //do the things with the db
+
+    //close connection
+    db.close()
+});
