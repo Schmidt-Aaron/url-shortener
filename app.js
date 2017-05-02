1c6163e9058f82941e59b4e7aa63184a07eec77b
Schmidt-Aaron <aschmidt808@gmail.com>
2017-05-02 10:56:37 -0700

    added db connection and basic express setup

--- app.js
+++ app.js
@@ -2,12 +2,15 @@
 var express = require('express');
 var app = express();
 
-//fire up mongo
-var mongo = require('mongodb');
-var MongoClient = mongo.MongoClient;
+app.use(express.static('public'));
 
-//where the database is
-var url = 'mongodb://localhost:27017/database_name';
+// //fire up mongo
+var MongoClient = require('mongodb').MongoClient;
+
+// //where the database is
+// var url = 'mongodb://localhost:27017/database_name';
+var url = "see onenote book;
+var currentEntry;
 
 MongoClient.connect(url, function(err, db){
     if(err) {
@@ -15,9 +18,30 @@ MongoClient.connect(url, function(err, db){
     } else {
         console.log('Connection established to', url);
     }
-
+    
     //do the things with the db
+    var collection = db.collection('urls');
+    // collection.insert( {
+    //     'original': 'http://google.com',
+    //     'new': '1'
+    // });
+
+    currentEntry = collection.count({'original'}, (err, count) => {
+        console.log(err, count);
+        
+        currentEntry = count;
+    });
+
+    console.log(currentEntry);
 
     //close connection
-    db.close()
+    db.close(() => {
+        console.log('Connection to db closed');
+    })
+});
+
+var port = process.env.port || 3000;
+app.set('port', port);
+app.listen(port, () => {
+    console.log(`Listening on ${port}.`)
 });
